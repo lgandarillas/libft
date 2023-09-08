94f5fdddb26b833cf6b9c84e8f304d482cbb7fff
lgandari <lgandaf@gmail.com>
2023-09-08 12:31:28 +0200

    add ft_strnstr.c, modified ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 17:12:00 by lgandari          #+#    #+#             */
-/*   Updated: 2023/08/26 17:54:38 by lganda           ###   ########.fr       */
+/*   Updated: 2023/09/04 11:25:05 by lganda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*lst;
+	int	pos;
 
-	lst = NULL;
-	if (c < 0 || c > 255)
-		c = (unsigned int)c;
-	else if (c == '\0')
-		return (NULL);
-	while (*s != '\0')
+	pos = ft_strlen(s);
+	while (pos >= 0)
 	{
-		if (*s == c)
-			lst = (char *)s;
-		s++;
+		if (s[pos] == c)
+			return ((char *)&s[pos]);
+		pos--;
 	}
-	if (lst != NULL && *lst == c)
-		return (lst);
-	else
-		return (NULL);
-}
-/*
-#include <stdio.h>
-#include <string.h>
-int	main(void)
-{
-	char	str[] = "Hola";
-	printf("\nF: %s", ft_strrchr(str, '\0'));
-	printf("\nO: %s", strrchr(str, '\0'));
 	return (0);
-}*/
+}
