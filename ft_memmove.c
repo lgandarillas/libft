3b86907548fdf7970d8b8226b45d34d7c01299e4
Luis Gandarillas Fernandez <lgandaf@gmail.com>
2023-09-11 16:45:26 +0200

    extra

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,31 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 15:26:45 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/11 15:27:26 by lgandari         ###   ########.fr       */
+/*   Updated: 2023/09/11 16:44:58 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	char	*saux;
+	char	*daux;
+	size_t	i;
 
+	saux = (char *)src;
+	daux = (char *)dst;
+	i = 0;
+	if (daux > saux)
+		while (len-- > 0)
+			daux[len] = saux[len];
+	else
+	{
+		while (i < len)
+		{
+			daux[i] = saux[i];
+			i++;
+		}
+	}
+	return ((char *)dst);
 }
