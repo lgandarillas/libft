79bbad93f8c577f6b96c6cbfd5252a10e7398ac1
Luis Gandarillas Fernandez <lgandaf@gmail.com>
2023-09-11 12:55:52 +0200

    ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,10 +6,23 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 12:06:41 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/11 12:11:20 by lgandari         ###   ########.fr       */
+/*   Updated: 2023/09/11 12:55:37 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}	
+	return ((char *)dst);
+}
