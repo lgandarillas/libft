ace6fbadf8e87ad0e57fb21339a931a232d49232
lgandari <lgandaf@gmail.com>
2023-09-14 18:13:26 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,10 +6,20 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 17:40:23 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/14 17:59:58 by lganda           ###   ########.fr       */
+/*   Updated: 2023/09/14 18:13:07 by lganda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_getstart(char const *s1, char const *set)
+{
+	int	len;
+	int	i;
+
+	len = ft_strlen(s1);
+	i = 0;
+	// Buscar un string dentro de otro
+}
 
 char	*ft_strtrim(char const *s1, char const *set);
