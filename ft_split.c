9b3fdb030dff6565258c4bbfacb0575c5541c223
Luis Gandarillas Fernandez <lgandaf@gmail.com>
2023-09-16 22:43:14 +0200

    modificaciones

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgandari <lgandari@student.42madrid.fr>    +#+  +:+       +#+        */
+/*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 12:37:44 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/16 14:15:15 by lgandari         ###   ########.fr       */
+/*   Created: 2023/09/16 22:02:50 by lgandari          #+#    #+#             */
+/*   Updated: 2023/09/16 22:30:55 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,41 @@ int	ft_countparts(char const *s, char c)
 	return (parts);
 }
 
-char	**ft_split(char const *s, char c);
+void	ft_freesplit(char **str, size_t parts)
+{
+	while (parts > 0)
+	{
+		free(str[parts]);
+		parts--;
+	}
+	free(str);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	char	**aux;
+	int		part;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	part = 0;
+	aux = ft_calloc(sizeof(char *), (ft_countparts(s, c) + 1));
+	if (!aux)
+		return (NULL);
+	while (s[i] != '\0' && (part < ft_countparts(s, c)))
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		j = i;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+		aux[part] = ft_substr(s, j, (i - j));
+		part++;
+	}
+	if (!aux)
+		ft_freesplit(aux, ft_countparts(s, c));
+	return (aux);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 10:05:34 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/14 11:43:50 by lganda           ###   ########.fr       */
+/*   Updated: 2023/09/16 22:43:02 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	i;
+	size_t	j;
 
 	if (!s)
 		return (NULL);
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
-	if (ft_strlen(s) < start)
-		len = 0;
-	else if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
-	ft_strlcpy(sub, s + start, len + 1);
-	((char *)sub)[len] = '\0';
+	i = start;
+	j = 0;
+	if (ft_strlen(s) <= start)
+		return (ft_strdup(""));
+	sub = (char *)malloc(len + 1);
+	while (j < len)
+	{
+		sub[j] = s[i];
+		i++;
+		j++;
+	}
+	sub[j] = '\0';
 	return (sub);
 }
