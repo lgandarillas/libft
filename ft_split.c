f0dceb5677ad7a931e43e7ce5b238ac1563b341b
lgandari <lgandaf@gmail.com>
2023-09-20 16:08:30 +0200

    fix: ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lganda <lgandari@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 22:02:50 by lgandari          #+#    #+#             */
-/*   Updated: 2023/09/20 15:32:11 by lganda           ###   ########.fr       */
+/*   Created: 2023/09/20 15:48:33 by lganda            #+#    #+#             */
+/*   Updated: 2023/09/20 16:06:09 by lganda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,48 +41,71 @@ void	ft_freesplit(char **str)
 	free(str);
 }
 
+size_t	calcpartsize(char const *s, char c)
+{
+	size_t	partsize;
+
+	partsize = 0;
+	while (*s != c && *s != '\0')
+	{
+		s++;
+		partsize++;
+	}
+	return (partsize);
+}
+
+int	ft_allocatepart(char **aux, size_t partsize, size_t i)
+{
+	aux[i] = (char *)malloc(sizeof(char) * (partsize + 1));
+	if (!aux)
+	{
+		ft_freesplit(aux);
+		return (1);
+	}
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t	i;
-	size_t	j;
 	char	**aux;
-	int		part;
+	size_t	parts;
+	size_t	partsize;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
 	i = 0;
-	part = 0;
-	aux = ft_calloc(sizeof(char *), ft_countparts(s, c) + 1);
-	if (aux == NULL)
-	{
-		free(aux);
+	parts = ft_countparts(s, c);
+	aux = (char **)malloc(sizeof(char *) * (parts + 1));
+	if (!aux)
 		return (NULL);
-	}
-	while (s[i] != '\0' && (part < ft_countparts(s, c)))
+	while (i != parts && *s)
 	{
-		while (s[i] != '\0' && s[i] == c)
-			i++;
-		j = i;
-		while (s[i] != '\0' && s[i] != c)
-			i++;
-		aux[part++] = ft_substr(s, j, (i - j));
+		while (*s == c)
+			s++;
+		partsize = calcpartsize(s, c);
+		if (ft_allocatepart(aux, partsize, i) != 0)
+			return (NULL);
+		ft_strlcpy(aux[i++], s, partsize + 1);
+		s += partsize;
 	}
+	aux[i] = NULL;
 	return (aux);
 }
 /*
 #include <stdio.h>
-int	main(void)
+int     main(void)
 {
-	char	**arr;
-	int	i;
+        char    **arr;
+        int     i;
 
-	i = 0;
-	arr = ft_split("hello!", ' ');
-	while (i < 1)
-	{
-		printf("%s", arr[i]);
-		printf("\n");
-		i++;
-	}
-	return (0);
+        i = 0;
+        arr = ft_split("hello ! que tal estas ", ' ');
+        while (i < 5)
+        {
+                printf("%s", arr[i]);
+                printf("\n");
+                i++;
+        }
+        return (0);
 }*/
