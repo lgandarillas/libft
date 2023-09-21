96a61dfa2c176136ec317d529022df3b8249b44c
lgandari <lgandaf@gmail.com>
2023-09-21 18:24:12 +0200

    add: lstmap

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,10 +6,19 @@
 /*   By: lganda <lgandari@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:52:38 by lganda            #+#    #+#             */
-/*   Updated: 2023/09/20 19:53:08 by lganda           ###   ########.fr       */
+/*   Updated: 2023/09/21 17:15:48 by lganda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		f(lst -> content);
+		lst = lst -> next;
+	}
+}
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,10 +6,76 @@
 /*   By: lganda <lgandari@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:53:14 by lganda            #+#    #+#             */
-/*   Updated: 2023/09/20 19:53:38 by lganda           ###   ########.fr       */
+/*   Updated: 2023/09/21 18:18:42 by lganda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_newnode(void *content)
+{
+	t_list	*new;
+
+	new = malloc (sizeof(t_list));
+	if (!new)
+	{
+		free(content);
+		return (NULL);
+	}
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}
+
+void	ft_freelst(t_list **lst, void (*del)(void *))
+{
+	t_list	*aux;
+	t_list	*next;
+
+	aux = *lst;
+	while (aux != NULL)
+	{
+		next = aux -> next;
+		del(aux -> content);
+		free(aux);
+		aux = next;
+	}
+	*lst = NULL;
+}
+
+void	ft_addback_lstmap(t_list **lst, t_list *new)
+{
+	t_list	*aux;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	aux = *lst;
+	while (aux -> next)
+		aux = aux -> next;
+	aux -> next = new;
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*aux;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new = NULL;
+	while (lst)
+	{
+		aux = ft_newnode(f(lst -> content));
+		if (aux == NULL)
+		{
+			ft_freelst(&new, del);
+			return (NULL);
+		}
+		ft_addback_lstmap(&new, aux);
+		lst = lst -> next;
+	}
+	return (new);
+}
